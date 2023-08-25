23f4dbc4dc91629db7cb72ef10cda9051ba0cd15
aryan-ajay <130049013+aryan-ajay@users.noreply.github.com>
2023-08-25 17:59:11 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,32 @@
+// Palindrome String
+
+//   Given a string S, check if it is palindrome or not.
+
+// Example 1:
+
+// Input: S = "abba"
+// Output: 1
+// Explanation: S is a palindrome
+// Example 2:
+
+// Input: S = "abc" 
+// Output: 0
+// Explanation: S is not a palindrome
+
+
+class Solution {
+    int isPalindrome(String str) {
+        // code here
+        for(int i=0; i<str.length()/2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)) {
+                return 0;
+                
+            }
+        }
+        return 1;
+    }
+}
+
+
+  
