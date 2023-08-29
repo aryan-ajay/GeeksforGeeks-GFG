7d0f072bbe06c131389fcc0ede3c2414d8a2d877
aryan-ajay <130049013+aryan-ajay@users.noreply.github.com>
2023-08-29 16:53:35 +0530

    Create RemoveDuplicateElementFromSortedLinkedList.java

---
+++ RemoveDuplicateElementFromSortedLinkedList.java
@@ -0,0 +1,31 @@
+// Remove duplicate element from sorted Linked List
+
+// Example 1:
+
+// Input:
+// LinkedList: 2->2->4->5
+// Output: 2 4 5
+// Explanation: In the given linked list 
+// 2 ->2 -> 4-> 5, only 2 occurs more 
+// than 1 time. So we need to remove it once.
+
+  class GfG
+{
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head)
+    {
+	// Your code here
+	Node node = head;
+	
+	while(node != null && node.next != null) {
+	    if(node.data == node.next.data) {
+	        node.next = node.next.next;
+	    } else {
+	        node = node.next;
+	    }
+	}
+	return head;
+	
+	
+    }
+}
